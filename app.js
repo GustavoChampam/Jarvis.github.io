cd2db7ba092f5752dbc85146aaf07fb835944486
Gustavo Champam <117557715+GustavoChampam@users.noreply.github.com>
2024-08-01 23:24:27 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ function speak(text) {
 }
 
 function wishMe() {
-    var day = new Date();
+    var day = new Date(); 
     var hour = day.getHours();
 
     if (hour >= 0 && hour < 12) {
-        speak("Good Morning Boss...");
+        speak("Bom dia Senhor...");
     } else if (hour >= 12 && hour < 17) {
-        speak("Good Afternoon Master...");
+        speak("Boa tarde senhor...");
     } else {
-        speak("Good Evening Sir...");
+        speak("Boa noite senhor...");
     }
 }
 
